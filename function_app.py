b0e7bee2828fa7cecf90c9f2da4a3ff408075ea0
LUISA VITÓRIA PENSO CAVAGNOLLO <luisacavagnollo@univille.br>
2026-09-15 20:13:09 -0300

    segundo commit

--- function_app.py
+++ function_app.py
@@ -10,3 +10,12 @@ def timer_trigger_turmaC(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
